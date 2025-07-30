195f855e1833dff5c4776169bfd946553f10d2fe
luis-villasana <luisvillasana020@gmail.com>
2025-07-30 16:58:25 -0500

    Have fn transformation example and decorator example.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from functools import wraps
+
 def multiply(x, y):
     return x * y
 
 def self_math(func):
+    @wraps(func)
     def inner_func(x):
         return func(x, x)
     
@@ -17,3 +20,13 @@ print(square(6)) # 36
 
 print(multiply(10, 10)) # 100
 print(square(10)) # 100
+
+# decorator way
+
+@self_math
+def multiply_fn(x, y):
+    return x * y
+
+print(multiply_fn(2)) # 2
+print(multiply_fn(7)) # 49
+print(multiply_fn(12)) # 144
